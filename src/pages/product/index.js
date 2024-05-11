202080fa6d11c0f75ad3b0e5e4c8fcc2b8dfe6ab
jyonitkapuriya <kapuriyajyonit755@gmail.com>
2024-05-11 13:22:00 +0530

    update code

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -12,6 +12,7 @@ import {
 } from "@material-tailwind/react";
 import ContactForm from "@/component/contact-form";
 import TimeLoader from "@/component/loader";
+import Footer from "@/component/footer";
 
 const Product = () => {
     const [product, setProduct] = useState([])
@@ -30,47 +31,63 @@ const Product = () => {
         }
     }, [type])
 
+    const convertName = (str) => {
+        return str.replace(/\b\w/g, function (char) {
+            return char.toUpperCase();
+        });
+    }
     if (!isLoaded) {
         return (
-            <div className="h-screen expert overflow-scroll no-scrollbar ">
-                <Navbar />
-                <div className="pt-24 px-8 lg:max-w-7xl mx-auto w-fit sm:px-8 grid grid-cols-4 gap-8">
-                    {product.map((item, index) => {
-                        return (
-                            <div key={index} className="border-2 cardBox border-solid lg:col-span-1 md:col-span-2 col-span-4 border-blue-400 p-8 rounded-lg shadow-xl">
-                                <div className="card">
-                                    <Card color="transparent" shadow={false} style={{ position: 'initial' }}>
-                                        <div className="mx-auto mt-0 ">
-                                            <Image
-                                                src={item.imageUrl}
-                                                alt={item.name}
-                                                width={768}
-                                                height={768}
-                                                className="w-32 h-32 lg:w-40 lg:h-40"
-                                            />
-                                        </div>
-                                        <CardBody className="p-0 flex flex-col items-center justify-center">
-                                            <a
-                                                href="#"
-                                                className="text-blue-gray-900 transition-colors hover:text-gray-800"
-                                            >
-                                                <Typography variant="h5" className="mb-2">
-                                                    {item.name}
-                                                </Typography>
-                                            </a>
+            <>
+                <div className="  expert overflow-scroll no-scrollbar">
+                    <div className="h-screen relative  overflow-scroll no-scrollbar ">
+                        <Navbar />
+                        <div className="pt-32 px-8 flex justify-center mb-8 animate-pulse lg:text-8xl text-5xl  font-sans font-semibold" style={{ color: '#07aadb' }}>
+                            {convertName(type.replace(/_/g, ' '))}
+                        </div>
+                        {/* <div className="flex flex-wrap justify-center text-gray-400 font-normal text-sm">
+                            <span onClick={() => router.push("/")} className="mr-3">Product</span> / <span className="ml-3">{convertName(type.replace(/_/g, ''))}</span>
+                        </div> */}
+                        <div className="  mt-16 lg:max-w-7xl mx-auto w-fit sm:px-8 grid grid-cols-6 gap-8">
+                            {product.map((item, index) => {
+                                return (
+                                    <div key={index} className="border-2 cardBox border-solid lg:col-span-2 md:col-span-3 col-span-6 border-blue-400 p-8 rounded-lg shadow-xl">
+                                        <div className="card">
+                                            <Card color="transparent" shadow={false} style={{ position: 'initial' }}>
+                                                <div className="mx-auto mt-0 ">
+                                                    <Image
+                                                        src={item.imageUrl}
+                                                        alt={item.name}
+                                                        width={768}
+                                                        height={768}
+                                                        className="w-32 h-32 lg:w-40 lg:h-40"
+                                                    />
+                                                </div>
+                                                <CardBody className="p-0 flex flex-col items-center justify-center">
+                                                    <a
+                                                        href="#"
+                                                        className="text-blue-gray-900 transition-colors hover:text-gray-800"
+                                                    >
+                                                        <Typography variant="h5" className="mb-2">
+                                                            {item.name}
+                                                        </Typography>
+                                                    </a>
 
-                                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full duration-300 " onClick={() => { router.push(`/product/details?id=${item?.id}`) }} >
-                                                View
-                                            </button>
-                                        </CardBody>
-                                    </Card>
-                                </div>
-                            </div>
-                        )
-                    })}
+                                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full duration-300 " onClick={() => { router.push(`/product/details?id=${item?.id}`) }} >
+                                                        View
+                                                    </button>
+                                                </CardBody>
+                                            </Card>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <ContactForm />
+                    </div>
                 </div>
-                <ContactForm />
-            </div>
+                <Footer />
+            </>
         )
     } else {
         return (
